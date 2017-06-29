e35f7fbece0b7735e224274e5156da4a69fdb9fc
Dan Davis <daniel.davis@nih.gov>
2017-06-29 15:33:03 -0400

    Add tests for elements in BaseMetadata

--- tests/tests.py
+++ tests/tests.py
@@ -1,6 +1,9 @@
 import os
 import imp
+import json
+
 import requests_mock
+import pytest
 
 ROLE_BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 RECONFIG_PATH = os.path.join(ROLE_BASE_DIR, 'files', 'awslogs-reconfig')
@@ -8,16 +11,57 @@ RECONFIG_PATH = os.path.join(ROLE_BASE_DIR, 'files', 'awslogs-reconfig')
 reconfig = imp.load_source('', RECONFIG_PATH)
 
 
+@pytest.fixture
+def metadata():
+    """
+    Isolates the dependence on the name within the module
+    """
+    return reconfig.EnhancedMetadata()
+
+
 def test_import():
-    '''
+    """
     This test basically passes if the module imports and has a main
-    '''
+    """
     assert 'main' in dir(reconfig)
 
 
-# NOTE: this requires a lot of mocking to test outside of AWS...
-def test_base_metadata():
+def test_region_property(metadata):
+    """
+    Test that the region property is working outide of an EC2 instance
+    """
     with requests_mock.mock() as m:
         m.get(reconfig.METADATA.format('placement/availability-zone'), text='us-west-1a')
-        basemeta = reconfig.BaseMetadata()
-        assert basemeta.region == 'us-west-1'
+        assert metadata.region == 'us-west-1'
+
+
+def test_instance_id_property(metadata):
+    """
+    Test that the instance_id property is working outside of an EC2 instance
+    """
+    expected_instance_id = 'i-8909234ge'
+    with requests_mock.mock() as m:
+        m.get(reconfig.METADATA.format('instance-id'), text=expected_instance_id)
+        assert metadata.instance_id == expected_instance_id
+
+
+def test_private_address_property(metadata):
+    """
+    Test the private_address property is working outside of an EC2 instance
+    """
+    expected_ipv4 = '10.100.97.88'
+    with requests_mock.mock() as m:
+        m.get(reconfig.METADATA.format('local-ipv4'), text=expected_ipv4)
+        assert metadata.private_address == expected_ipv4
+
+
+def test_iam_info_property(metadata):
+    """
+    Test that the IAM Info property is working outside of an EC2 instance
+    """
+    expected_iaminfo = json.dumps({'notreal': {'notrealiam': 'role'}})
+    with requests_mock.mock() as m:
+        m.get(reconfig.METADATA.format('iam/info'), text=expected_iaminfo)
+        actual_iaminfo = metadata.iam_info
+        assert isinstance(actual_iaminfo, dict)
+        assert actual_iaminfo['notreal']['notrealiam'] == 'role'
